9aa76af53fbe86081503a25db84f7e5b6f69e81d
Angelina Momin <amomin2630@outlook.com>
2023-09-02 14:07:08 +0200

    Format with black

--- utilities/initialize_graph_with_nodes.py
+++ utilities/initialize_graph_with_nodes.py
@@ -1,10 +1,15 @@
 import numpy as np
 import networkx as nx
 
-def initialize_graph_with_nodes(list_factory_coords: np.ndarray, list_warehouse_coords: np.ndarray, list_distribution_center_coords: np.ndarray)->nx.Graph:
+
+def initialize_graph_with_nodes(
+    list_factory_coords: np.ndarray,
+    list_warehouse_coords: np.ndarray,
+    list_distribution_center_coords: np.ndarray,
+) -> nx.Graph:
     """
     Given building coords, initializes and returns a networkx graph with the nodes for each building.
-    The nodes contains information on the coords as well as the building types.
+    The nodes contains information on the position coords, the building type and the color of the node.
 
     Args:
         list_factory_coords (np.ndarray): List of factory coords
@@ -13,11 +18,15 @@ def initialize_graph_with_nodes(list_factory_coords: np.ndarray, list_warehouse_
 
     Returns:
         nx.Graph: A networkx graph containing all the nodes
-    """   
+    """
 
-    factory_nodes = { f"F{index}": coords for index, coords in enumerate(list_factory_coords)}
-    warehouse_nodes = { f"W{index}": coords for index, coords in enumerate(list_warehouse_coords)}
-    distribution_center_node = { "DC": list_distribution_center_coords }
+    factory_nodes = {
+        f"F{index}": coords for index, coords in enumerate(list_factory_coords)
+    }
+    warehouse_nodes = {
+        f"W{index}": coords for index, coords in enumerate(list_warehouse_coords)
+    }
+    distribution_center_node = {"DC": list_distribution_center_coords}
     all_nodes = factory_nodes | warehouse_nodes | distribution_center_node
 
     # * Initialize graph
