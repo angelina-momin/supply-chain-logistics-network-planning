07030c0c15573bccaf5593a356a002e0ab511170
speeder-coder <116162180+speeder-coder@users.noreply.github.com>
2023-09-02 16:33:44 +0200

    Add utility/initialize graph with nodes (#6)
    * create initialize_graph_with_nodes
    
    * Format with black
    
    * Add comments for readability

---
+++ utilities/initialize_graph_with_nodes.py
@@ -0,0 +1,49 @@
+import numpy as np
+import networkx as nx
+
+
+def initialize_graph_with_nodes(
+    list_factory_coords: np.ndarray,
+    list_warehouse_coords: np.ndarray,
+    list_distribution_center_coords: np.ndarray,
+) -> nx.Graph:
+    """
+    Given building coords, initializes and returns a networkx graph with the nodes for each building.
+    The nodes contains information on the position coords, the building type and the color of the node.
+
+    Args:
+        list_factory_coords (np.ndarray): List of factory coords
+        list_warehouse_coords (np.ndarray): List of warehouse coords
+        list_distribution_center_coords (np.ndarray): List of distribution center coords
+
+    Returns:
+        nx.Graph: A networkx graph containing all the nodes
+    """
+
+    # * Create dictionary of points
+    factory_nodes = {
+        f"F{index}": coords for index, coords in enumerate(list_factory_coords)
+    }
+    warehouse_nodes = {
+        f"W{index}": coords for index, coords in enumerate(list_warehouse_coords)
+    }
+    distribution_center_node = {"DC": list_distribution_center_coords}
+    all_nodes = factory_nodes | warehouse_nodes | distribution_center_node
+
+    # * Initialize graph
+    network_graph = nx.Graph()
+
+    # Add nodes with their attributes
+    for node, coords in all_nodes.items():
+
+        # Add building_type
+        if node == "DC":
+            building_type = "distribution center"
+        elif node.startswith("W"):
+            building_type = "warehouse"
+        else:
+            building_type = "factory"
+
+        network_graph.add_node(node, pos=coords, building_type=building_type)
+
+    return network_graph
