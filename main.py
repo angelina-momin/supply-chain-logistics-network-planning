726c0c6402261d984885fa24b9d9a7de1c002542
Angelina Momin <amomin2630@outlook.com>
2023-09-06 10:50:32 +0200

    Added metric closure to main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 import numpy as np
 import networkx as nx
 from networkx.algorithms.approximation.steinertree import steiner_tree
+from networkx.algorithms.approximation.steinertree import metric_closure
 import math
 
 from utilities.create_connected_graph import create_connected_graph
@@ -51,7 +52,13 @@ def city_planning(
 
     initial_graph = initialize_graph_with_nodes(list_factory_coords, list_warehouse_coords, list_distribution_center_coords)
 
+    # * Creating a graph with all possible roads
     connected_graph = create_connected_graph(initial_graph, dict_route_costs_per_km)
+
+    # * Creating metric closure of the connected graph
+    # The metric closure of the connected graph is a complete graph where each edge which each edge is weighted by the 
+    # shortest path distance between the nodes in the connected graph
+    metric_closure_graph = metric_closure(connected_graph)
 
     # STEP 1) Create a steiner tree
     # Terminal_nodes: all houses, city_centre
